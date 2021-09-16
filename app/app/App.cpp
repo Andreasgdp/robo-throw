6b8a2d1d317e5bb57817b1c8c3f87f5b99ee4f9b
Andreasgdp <andreasgdp@gmail.com>
2021-09-16 11:41:12 +0200

    Added first stuff to App

--- app/app/App.cpp
+++ app/app/App.cpp
@@ -1,6 +1,17 @@
 #include "App.h"
 
-App::App()
+App::App(std::string IP) : roboConn(IP)
 {
+    this->IP = IP;
+}
 
+void App::initializeApp(const cv::Mat &calibrationImg)
+{
+    if (!this->roboConn.isConnected()) throw "Connection could not be established with ip: " + this->IP;
+
+    if (calibrationImg.cols == 0 && calibrationImg.rows == 0) {
+        // Use imageProcessing to get image from camera
+    }
+
+    // use imageProcessing to calibrate camera
 }
--- app/app/App.h
+++ app/app/App.h
@@ -1,11 +1,32 @@
 #ifndef APP_H
 #define APP_H
-
+#include <vector>
+#include <string>
+#include "opencv2/opencv.hpp"
+#include "../imageProcessing/ImageProcessing.h"
+#include "../robotConnection/RobotConnection.h"
 
 class App
 {
 public:
-    App();
+    App(std::string IP);
+    void initializeApp(const cv::Mat &calibrationImg = cv::Mat());
+    void findAndGrabObject(const cv::Mat &objectImg = cv::Mat());
+
+
+private:
+    std::string IP;
+    std::vector<double> jointPoses;
+    ImageProcessing imgProcessor;
+    RobotConnection roboConn;
+    // add member variable for jointPoseGetter
+    // add member variable for gripperHandling
+    // add member variable for api
+    // add member variable for image
+    // add member variable for processed image
+    // add member variable for object posision
+    // add member variable for target pos
+
 };
 
 #endif // APP_H
