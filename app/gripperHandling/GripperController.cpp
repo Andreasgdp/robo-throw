9763fc95247cb2458924949e5bcc317d9f0aa919
Andreasgdp <andreasgdp@gmail.com>
2021-09-20 14:35:51 +0200

    Getting state and setting values of gripper

--- app/gripperHandling/GripperController.cpp
+++ app/gripperHandling/GripperController.cpp
@@ -12,5 +12,25 @@ void GripperController::close()
 
 void GripperController::open()
 {
-	this->gripper.open();
+    this->gripper.open();
+}
+
+rl::hal::WeissWsg50::GraspingState GripperController::getGraspingState()
+{
+    return this->gripper.getGraspingState();
+}
+
+void GripperController::setAcceleration(const float &acceleration)
+{
+    this->gripper.doSetAcceleration(acceleration);
+}
+
+void GripperController::setForceLimit(const float &force)
+{
+    this->gripper.doSetForceLimit(force);
+}
+
+void GripperController::setSoftLimits(const float &limitMinus, const float &limitPlus)
+{
+    this->gripper.doSetSoftLimits(limitMinus, limitPlus);
 }
--- app/gripperHandling/GripperController.h
+++ app/gripperHandling/GripperController.h
@@ -10,6 +10,10 @@ public:
     GripperController(const std::string &address="192.168.1.20", const unsigned short int &port=1000, const float &acceleration=3.0f, const float &forceLimit=40.0f, const unsigned int &period=10);
     void close();
     void open();
+    rl::hal::WeissWsg50::GraspingState getGraspingState();
+    void setAcceleration(const float &acceleration);
+    void setForceLimit(const float &force);
+    void setSoftLimits(const float &limitMinus, const float &limitPlus);
 
 private:
     rl::hal::WeissWsg50 gripper;
