919ff9f88ee479baf5b85246c2d8d51e3eeb7f7f
Andreasgdp <andreasgdp@gmail.com>
2021-09-21 09:06:03 +0200

    Formatted constructor

--- app/gripperHandling/GripperController.cpp
+++ app/gripperHandling/GripperController.cpp
@@ -1,6 +1,12 @@
 #include "GripperController.h"
 
-GripperController::GripperController(const ::std::string &address, const unsigned short int &port, const float &acceleration, const float &forceLimit, const unsigned int &period) : gripper(address, port, acceleration, forceLimit, period)
+GripperController::GripperController(
+        const std::string &address,
+        const unsigned short int &port,
+        const float &acceleration,
+        const float &forceLimit,
+        const unsigned int &period)
+    : gripper(address, port, acceleration, forceLimit, period)
 {
 
 }
