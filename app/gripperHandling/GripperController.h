7caedda164fbbd63ce98a5d73a2e8f50c8681f74
Piekut <tobias@piekut.dk>
2021-09-21 09:41:57 +0200

    Added docstrings in gripper.h

--- app/gripperHandling/GripperController.h
+++ app/gripperHandling/GripperController.h
@@ -14,11 +14,38 @@ public:
             const float &forceLimit=40.0f,
             const unsigned int &period=10);
     void close();
+    /*
+     * @param Closes the gripper
+     */
     void open();
+    /*
+     * @param Opens the gripper
+     */
     rl::hal::WeissWsg50::GraspingState getGraspingState();
+    /*
+     * @param Gets the graspingstate
+     * @return Graspingstate
+     */
     void setAcceleration(const float &acceleration);
+    /*
+     * @param Sets the acceleration
+     */
+    float getAcceleration();
+    /*
+     * @return Acceleration of the gripper
+     */
     void setForceLimit(const float &force);
+    /*
+     * @param Sets the forcelimit of the gripper
+     */
+    float getForceLimit();
+    /*
+     * @return Forcelimit of the gripper
+     */
     void setSoftLimits(const float &limitMinus, const float &limitPlus);
+    /*
+     * @param Sets the softlimits of the gripper
+     */
 
 private:
     rl::hal::WeissWsg50 gripper;
