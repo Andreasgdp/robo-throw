9874ce469b5f5b6ecc9b0701add4655c4bfc4159
Jacob Brengdahl Larsen <jlarsen2107@gmail.com>
2021-09-27 10:52:40 +0200

    Gripper class tested and adapted

--- app/gripperHandling/GripperController.cpp
+++ app/gripperHandling/GripperController.cpp
@@ -6,37 +6,62 @@ GripperController::GripperController(
         const float &acceleration,
         const float &forceLimit,
         const unsigned int &period)
-    : gripper(address, port, acceleration, forceLimit, period)
-{
-
+    : _gripper(address, port, acceleration, forceLimit, period) {
+    connect();
 }
 
-void GripperController::close()
-{
-	this->gripper.close();
+GripperController::~GripperController() {
+    disconnect();
 }
 
-void GripperController::open()
-{
-    this->gripper.open();
+void GripperController::connect() {
+    this->_gripper.open();
+    this->_gripper.start();
 }
 
-rl::hal::WeissWsg50::GraspingState GripperController::getGraspingState()
-{
-    return this->gripper.getGraspingState();
+void GripperController::disconnect() {
+    this->_gripper.stop();
+    this->_gripper.close();
 }
 
-void GripperController::setAcceleration(const float &acceleration)
-{
-    this->gripper.doSetAcceleration(acceleration);
+void GripperController::close() {
+    this->_gripper.doPrePositionFingers(0.01f, 0.05f, false, true);
 }
 
-void GripperController::setForceLimit(const float &force)
-{
-    this->gripper.doSetForceLimit(force);
+void GripperController::open(const float &speed) {
+    this->_gripper.doPrePositionFingers(0.1f, speed, false, true);
 }
 
-void GripperController::setSoftLimits(const float &limitMinus, const float &limitPlus)
-{
-    this->gripper.doSetSoftLimits(limitMinus, limitPlus);
+void GripperController::moveHome() {
+    this->_gripper.doHomingMotion();
+}
+
+rl::hal::WeissWsg50::GraspingState GripperController::_getGraspingState() {
+    return this->_gripper.getGraspingState();
+}
+
+std::string GripperController::getGraspingState() {
+    const char* state[] = {
+        "GRASPING_STATE_IDLE",
+        "GRASPING_STATE_GRIPPING",
+        "GRASPING_STATE_NO_PART_FOUND",
+        "GRASPING_STATE_PART_LOST",
+        "GRASPING_STATE_HOLDING",
+        "GRASPING_STATE_RELEASING",
+        "GRASPING_STATE_POSITIONING",
+        "GRASPING_STATE_ERROR"
+    };
+    return state[_getGraspingState()];
+}
+
+void GripperController::setAcceleration(const float &acceleration) {
+    this->_gripper.doSetAcceleration(acceleration);
+}
+
+void GripperController::setForceLimit(const float &force) {
+    this->_gripper.doSetForceLimit(force);
+}
+
+void GripperController::setSoftLimits(const float &limitMinus, const float &limitPlus) {
+    this->_gripper.doSetSoftLimits(limitMinus, limitPlus);
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include "./robotConnection/RobotConnection.h"
 #include "./app/App.h"
+#include "./gripperHandling/GripperController.h"
 
 using namespace std;
 
 int main()
 {
+
     // TODO: use IP of robot.
     App app("130.226.87.132");
     app.calibrateCam();
     app.findAndGrabObject();
     std::vector<double> goalPos;
     app.throwObject(goalPos);
+
+
+    GripperController myGripper("192.168.100.11", 1000);
+    try {
+        myGripper.moveHome();
+
+        myGripper.close();
+        myGripper.open();
+
+    }  catch (exception& e) {
+        cout << e.what() << endl;
+        return 1;
+    }
 
     return 0;
 }
