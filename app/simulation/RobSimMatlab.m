ca7d26a74f5da03dfe5e261300d06935580c66bc
Tobias Krongaard Piekut <tobias@piekut.dk>
2021-10-05 09:35:18 +0200

    Added a jacobian calculator for a specific config

--- app/simulation/RobSimMatlab.m
+++ app/simulation/RobSimMatlab.m
@@ -2,7 +2,7 @@ clear
 close all
 
 % uncomment if you need the documentation
-%doc loadrobot
+doc loadrobot
 
 % Loads a robot
 ur = loadrobot("universalUR5",'DataFormat','row');
@@ -18,6 +18,10 @@ tform = getTransform(ur,randConfig,'tool0','base');
 % Input a config to simulate
 simConfig = [0 0 0 0 0 0];
 simTform = getTransform(ur,simConfig,'tool0','base');
+
+% Calc a jacobian for a specific config
+geoJacob = geometricJacobian(ur,simConfig,'tool0');
+disp(geoJacob);
 
 % Calc the inverse kinematics
 ik = inverseKinematics('RigidBodyTree',ur);
