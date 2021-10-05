970b7acfc4d30b1c573556ea16d87531772f157e
Andreasgdp <andreasgdp@gmail.com>
2021-10-05 10:21:08 +0200

    Added a way to display jacobian

--- app/jointPoseGetter/JointPoseGetter.h
+++ app/jointPoseGetter/JointPoseGetter.h
@@ -9,6 +9,8 @@ public:
 	JointPoseGetter(/* args */);
 	~JointPoseGetter();
     std::vector<std::vector<double>> jacobian(double q1, double q2, double q3, double q4, double q5, double q6);
+    void showJacobian();
 private:
+    std::vector<std::vector<double>> j;
 };
 
--- app/main.cpp
+++ app/main.cpp
@@ -12,15 +12,7 @@ int main()
     JointPoseGetter j;
 
     vector<vector<double>> test = j.jacobian(1, 1, 1, 1, 1, 1);
-
-    for (int i = 0; i < test.size(); i++)
-    {
-        for (int j = 0; j < test[i].size(); j++)
-        {
-            cout << test[i][j] << ", ";
-        }
-        cout << endl;
-    }
+    j.showJacobian();
 
     return 0;
 }
