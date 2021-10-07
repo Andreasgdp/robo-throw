6ef4f98583e39e378b2686657902b2fdfb140963
Andreasgdp <andreasgdp@gmail.com>
2021-10-07 10:52:57 +0200

    Added moving and calibration to simulation class

--- app/simulation/Simulation.h
+++ app/simulation/Simulation.h
@@ -3,7 +3,7 @@
 
 #include "opencv2/opencv.hpp"
 #include "../robotConnection/RobotConnection.h"
-
+#include "../imageProcessing/ImageProcessing.h"
 
 /*
     @brief Class to take possible commands to robot and simulate them
@@ -14,10 +14,17 @@ class Simulation
 {
 public:
     Simulation(std::string IP);
+    bool calibrateCam();
+    bool moveSuccess(const std::vector<double> &jointPoses, double speed, double acceleration);
+
 
 
 private:
     RobotConnection roboConn;
+    ImageProcessing imgProc;
+
+    bool hasFinishedMoving(const std::vector<double> &pos);
+    bool hasMovedToPos(const std::vector<double> &pos);
 };
 
 #endif // SIMULATION_H
